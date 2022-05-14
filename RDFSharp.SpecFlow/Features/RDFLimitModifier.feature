a286250b5aecaa1b80b0c74c2f3725dd0eea307b
Pongrácz Ádám <9999adam99@gmail.com>
2022-05-14 17:50:22 +0200

    Created SpecFlow tests to the RDFLimitModifier class.

---
+++ RDFSharp.SpecFlow/Features/RDFLimitModifier.feature
@@ -0,0 +1,10 @@
+﻿Feature: RDFLimitModifier
+	RDFLimitModifier is a modifier which applies an upper-bound counter to the number of query results to be considered
+
+Scenario: Create valid RDFLimitModifier
+	When create a RDFLimitModifier with a limit of 10
+	Then the modifier is successfully created
+
+Scenario: Create invalid RDFLimitModifier
+	When create a RDFLimitModifier with a limit of -1
+	Then the program sould throw an RDFQueryException 
---
+++ RDFSharp.SpecFlow/Steps/RDFLimitModifierSteps.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RDFSharp.Model;
+using RDFSharp.Query;
+using System;
+using System.Data;
+using TechTalk.SpecFlow;
+
+namespace RDFSharp.SpecFlow.Steps
+{
+    [Binding]
+    public class RDFLimitModifierSteps
+    {
+        private readonly ScenarioContext _scenarioContext;
+        private RDFLimitModifierSteps(ScenarioContext scenarioContext)
+        {
+            this._scenarioContext = scenarioContext;
+        }
+
+        [When(@"create a RDFLimitModifier with a limit of (.*)")]
+        public void WhenCreateARDFLimitModifierWithALimitOf(int limit)
+        {
+            try
+            {
+                _scenarioContext["Modifier"] = new RDFLimitModifier(limit);
+                _scenarioContext["Limit"] = limit;
+            }
+            catch (RDFQueryException e)
+            {
+                _scenarioContext["Exception"] = e;
+            }
+        }
+        
+        [Then(@"the modifier is successfully created")]
+        public void ThenTheModifierIsSuccessfullyCreated()
+        {
+            var modifier = (RDFLimitModifier)_scenarioContext["Modifier"];
+            var limit = (int)_scenarioContext["Limit"];
+            Assert.IsNotNull(modifier);
+            Assert.IsTrue(modifier.Limit == limit);
+            Assert.IsTrue(modifier.ToString().Equals("LIMIT " + limit));
+        }
+        
+        [Then(@"the program sould throw an RDFQueryException")]
+        public void ThenTheProgramSouldThrowAnRDFQueryException()
+        {
+            var exception = (RDFQueryException)_scenarioContext["Exception"];
+            Assert.IsNotNull(exception);
+        }
+    }
+}
