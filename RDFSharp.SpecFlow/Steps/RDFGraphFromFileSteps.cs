e3f834d7ef403bc94a0aff71d9b8a9f01fd0e8d5
Pongrácz Ádám <9999adam99@gmail.com>
2022-05-17 18:16:26 +0200

    Tested the creation of RDFGraph from file

---
+++ RDFSharp.SpecFlow/Steps/RDFGraphFromFileSteps.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RDFSharp.Model;
+using System;
+using TechTalk.SpecFlow;
+
+namespace RDFSharp.SpecFlow.Steps
+{
+    [Binding]
+    public class RDFGraphFromFileSteps
+    {
+        private readonly ScenarioContext _scenarioContext;
+        private RDFGraphFromFileSteps(ScenarioContext scenarioContext)
+        {
+            this._scenarioContext = scenarioContext;
+        }
+
+        [Given(@"an RDF graph in the file ""(.*)""")]
+        public void GivenAnRDFGraphInTheFile(string fileName)
+        {
+            _scenarioContext["Path"] = AppDomain.CurrentDomain.BaseDirectory + @"TestFiles\" + fileName;
+        }
+        
+        [When(@"create a graph from the file")]
+        public void WhenCreateAGraphFromTheFile()
+        {
+            var path = (string)_scenarioContext["Path"];
+            try
+            {
+                _scenarioContext["Graph"] = RDFGraph.FromFile(RDFModelEnums.RDFFormats.RdfXml, path);
+            }
+            catch (Exception e)
+            {
+                _scenarioContext["Exception"] = e;
+            }
+        }
+        
+        [Then(@"the graph sould be succesfully created")]
+        public void ThenTheGraphSouldBeSuccesfullyCreated()
+        {
+            var graph = _scenarioContext["Graph"];
+            Assert.IsNotNull(graph);
+        }
+
+        [Then(@"the program should throw an Exception, because of invalid file")]
+        public void ThenTheProgramShouldThrowAnExceptionBecauseOfInvalidFile()
+        {
+            var exception = _scenarioContext["Exception"];
+            Assert.IsNotNull(exception);
+        }
+    }
+}
